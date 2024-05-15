2ddba5753f920e271da887da2229b182f47f329f
Le Cao Minh Tri <103999209+LCMTRI@users.noreply.github.com>
2024-05-15 16:18:15 +0700

    config for deployment

--- handlers/health_handler.go
+++ handlers/health_handler.go
@@ -1,20 +1,15 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
 func HealthCheckHandler(c echo.Context) error {
-	cookie, err := c.Cookie("jwt")
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "error get jwt cookie")
-	}
-	fmt.Println("cookie: ", cookie.Value)
+
 	// if err != nil {
 	// 	return c.JSON(http.StatusInternalServerError, err)
 	// }
-	return c.String(http.StatusOK, "oke")
+	return c.String(http.StatusOK, "Health check ok")
 }
